0b53a5f4863186210a03773cea4c0ca229460248
KiranNinave <ninavekirankn@gmail.com>
2018-11-17 12:54:33 +0530

    bootstrap added theme builded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header branding="Contact Manager" />
+
+        <div className="container">
+          <Contact
+            name="kiran ninave"
+            email="kiran@gmail.com"
+            phone="1234-5678-90"
+          />
+          <Contact
+            name="kiran ninave"
+            email="kiran@gmail.com"
+            phone="1234-5678-90"
+          />
+        </div>
       </div>
     );
   }
