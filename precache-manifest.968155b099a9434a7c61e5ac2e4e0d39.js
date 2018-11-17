f0131c4aa4499d868cb286ba6145368094de09ff
KiranNinave <ninavekirankn@gmail.com>
2018-11-17 13:02:22 +0530

    Updates

---
+++ precache-manifest.968155b099a9434a7c61e5ac2e4e0d39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "f752a05c7eea0b53fb14",
+    "url": "/contact-manager/static/js/main.f752a05c.chunk.js"
+  },
+  {
+    "revision": "1a028006372690967eed",
+    "url": "/contact-manager/static/js/1.1a028006.chunk.js"
+  },
+  {
+    "revision": "f752a05c7eea0b53fb14",
+    "url": "/contact-manager/static/css/main.1af24ae9.chunk.css"
+  },
+  {
+    "revision": "1a028006372690967eed",
+    "url": "/contact-manager/static/css/1.b8b00534.chunk.css"
+  },
+  {
+    "revision": "15f075d5e7ff797dfe8568ab28e30ec8",
+    "url": "/contact-manager/index.html"
+  }
+];
